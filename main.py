85dc4c78fdd14b0256552b9a55153a356dee8685
khunfloat <khunfloat@gmail.com>
2022-11-24 21:49:07 +0700

    create new endpoint /location

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def index():
 @app.route("/items-list")
 def items_list():
     return {"items" : ["A", "B", "C", "D", "E", "F"]}
+
+@app.route("/location")
+def location():
+    return "We have a nice location for you!"
