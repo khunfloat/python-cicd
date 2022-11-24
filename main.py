86dd91bccd6751e56c86ca994932780e6dd428da
khunfloat <khunfloat@gmail.com>
2022-11-24 21:35:58 +0700

    create new endpoint /dev

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Python CI/CD with Github actions"
+
+@app.route("/dev")
+def index():
+    return "This feature is developed in dev branch"
